abaa748b5ffbaec9e1ac852964b1d3231d5203e9
Aniket Jadhav <aniket.jadhav20703@gmail.com>
2025-07-16 23:05:45 +0530

    fix: remove trailing slash in CORS

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',
   'https://copypaste-ai-authbackend.onrender.com',
-  'https://dancing-mousse-c5c6d5.netlify.app/', // ✅ Add Netlify frontend
+  'https://dancing-mousse-c5c6d5.netlify.app',
+ // ✅ Add Netlify frontend
 ];
 
 
